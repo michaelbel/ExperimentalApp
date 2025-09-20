059b3f9063f98a4fb7fc862a490154e35acebbe0
michaelbel <michael-bel@outlook.com>
2025-09-20 12:39:38 +0300

    Update project

--- app/src/main/kotlin/org/michaelbel/kotlin_data_class/MainActivity.kt
+++ app/src/main/kotlin/org/michaelbel/kotlin_data_class/MainActivity.kt
@@ -5,24 +5,19 @@ package org.michaelbel.kotlin_data_class
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.navigation.compose.composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.compose.AndroidFragment
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.michaelbel.kotlin_data_class.ui.AppTheme
 
@@ -34,38 +29,17 @@ class MainActivity: FragmentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                val navController = rememberNavController()
-                var selectedTab by remember { mutableStateOf("Compose") }
-
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = { TopAppBar(title = { Text(text = "Plurals") }) },
-                    bottomBar = {
-                        NavigationBar {
-                            NavigationBarItem(
-                                selected = selectedTab == "View",
-                                onClick = { selectedTab = "View" },
-                                label = { Text("View") },
-                                icon = {}
-                            )
-                            NavigationBarItem(
-                                selected = selectedTab == "Compose",
-                                onClick = { selectedTab = "Compose" },
-                                label = { Text("Compose") },
-                                icon = {}
-                            )
-                        }
-                    }
+                    topBar = { TopAppBar(title = { Text(text = stringResource(R.string.app_name)) }) },
                 ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = selectedTab,
+                    Box(
                         modifier = Modifier.padding(innerPadding).fillMaxSize()
-                    ) {
-                        composable("Compose") { PluralsComposable() }
-                    }
+                    )
                 }
             }
         }
     }
 }
+
+// todo фиолетовый и рыжий цвет интерфейсов
